e163e7dea107921f8f6b63499d18a8748d110376
NadG17 <nadish23334@iiitd.ac.in>
2025-07-28 10:47:09 +0530

    Update aes.h

--- include/aes.h
+++ include/aes.h
@@ -2,3 +2,5 @@
 #include <vector>
 #include "common.h"
 void aes_encrypt_gpu(const std::vector<uint8_t>&, std::vector<uint8_t>&, const KeyBundle&, const Config&);
+__device__ void aes_rounds(uint8_t state[16], const uint8_t* roundKeys);
+
